5309bff1dcd49d7deb4a19676b86593c38764f48
ayoub anbara <ayoub1996anbara@gmail.com>
2020-02-26 13:26:49 +0100

    adding apointment page

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -15,6 +15,56 @@ $(function() {
     });
     $("input[data-column=4]").hide();
 });
+$(function() {
+    $(".tableAppointments").tablesorter({
+        widthFixed: true,
+        widgets: ["zebra", "filter"],
+        widgetOptions: {
+            filter_cssFilter: '',
+            filter_childRows: false,
+            filter_hideFilters: false,
+            filter_ignoreCase: true,
+            filter_reset: '.reset',
+            filter_saveFilters: true,
+            filter_searchDelay: 300,
+            filter_startsWith: false,
+        }
+    });
+    $("input[data-column=3]").hide();
+});
+
+// to include in an file html 
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
+        }
+    }
+}
 
 (function($) {
     "use strict"; // Start of use strict
